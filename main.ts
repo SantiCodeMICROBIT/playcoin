f4fbf3f264868f50137feca7d5aeee659f5f6301
SantiCodeMICROBIT <114035759+SantiCodeMICROBIT@users.noreply.github.com>
2022-09-23 17:11:01 -0500

    REVAMPED

--- main.ts
+++ main.ts
@@ -2,20 +2,44 @@ enum RadioMessage {
     message1 = 49434
 }
 radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("RECIVED")
     basic.showString("" + (receivedNumber))
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showString("P-COIN")
+    basic.showString("TOTAL")
     basic.showNumber(PLAYCOIN)
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.showString("SENT")
     radio.sendString("" + (PLAYCOIN))
 })
 input.onButtonPressed(Button.B, function () {
     PLAYCOIN += -50
+    basic.showString("UPGRADED")
     upgrade = 1
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # # # #
+        . . . . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
     if (upgrade == 0) {
         PLAYCOIN += 1
     } else if (upgrade == 1) {
