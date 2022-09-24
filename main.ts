1246e3a8153bfbe1de7f9db57fa9a5a40e00fc84
SantiCodeMICROBIT <114035759+SantiCodeMICROBIT@users.noreply.github.com>
2022-09-23 20:12:57 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,19 +4,47 @@ enum RadioMessage {
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showString("RECIVED")
     basic.showString("" + (receivedNumber))
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.A, function () {
     basic.showString("TOTAL")
     basic.showNumber(PLAYCOIN)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("SENT")
     radio.sendString("" + (PLAYCOIN))
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     PLAYCOIN += -50
     basic.showString("UPGRADED")
     upgrade = 1
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
@@ -62,5 +90,12 @@ basic.forever(function () {
     if (PLAYCOIN < 0) {
         upgrade = 0
         basic.showString("DEBT")
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            . . . . .
+            `)
     }
 })
